9820caeff96eb41c1579f92e070921cdca05062f
Ben Wencke <benwencke@gmail.com>
2014-07-26 20:08:09 -0400

    penis

--- cast-custom-receiver/js/view.js
+++ cast-custom-receiver/js/view.js
@@ -1,7 +1,8 @@
 // utility function to display the text message in the input field
-function displayText(text) {
+function displayText(textMsg) {
     document.getElementById("message").style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-    console.log(text);
-    document.getElementById("message").innerHTML=text;
-    window.castReceiverManager.setApplicationState(text);
+    console.log(textMsg);
+    //document.getElementById("message").innerHTML=text;
+    text("You said: "+textMsg);
+    window.castReceiverManager.setApplicationState(textMsg);
 };
