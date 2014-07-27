1fd2cf89ef14c54fb9ff63c5ef0b726c54105266
Ben Wencke <benwencke@gmail.com>
2014-07-26 23:40:11 -0400

    penis

--- cast-custom-receiver/js/chromecast/chromecast.js
+++ cast-custom-receiver/js/chromecast/chromecast.js
@@ -1,6 +1,7 @@
 // utility function to display the text message in the input field
 function displayText(textMsg) {
     document.getElementById("message").style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    window.viewModel.text(textMsg);
     console.log(textMsg);
     //document.getElementById("message").innerHTML=text;
     window.castReceiverManager.setApplicationState(textMsg);
