714bfef459374a5b07ba34b95a0759fdaff01f17
Ben Wencke <benwencke@gmail.com>
2014-07-27 14:23:01 -0400

    all cards submitted

--- cast-custom-receiver/js/app.js
+++ cast-custom-receiver/js/app.js
@@ -39,8 +39,6 @@ $(document).ready(function() {
 		// 	}
 		// };
 
-		self.czarFlipped = ko.observable(false);
-
 		self.chooser = ko.observable(0);
 		self.text = ko.observable("TALK TO ME, MOTHERFUCKER!!!!!!!!!");
 		self.connections = ko.observable(0);
